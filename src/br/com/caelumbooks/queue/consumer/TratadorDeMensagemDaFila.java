7ed46cb414183baadf73f4f3b73a1750c160379b
alexandregama <alexandre.gama.lima@gmail.com>
2015-11-11 20:40:23 -0200

    Implementa consumidor e tratador de mensagens da fila

---
+++ src/br/com/caelumbooks/queue/consumer/TratadorDeMensagemDaFila.java
@@ -0,0 +1,20 @@
+package br.com.caelumbooks.queue.consumer;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+public class TratadorDeMensagemDaFila implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		TextMessage msg = (TextMessage) message;
+		try {
+			System.out.println(msg.getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
